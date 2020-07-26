493f17565e425977ea8732d67122ac71d89a8d49
Masafumi24 <61171572+Masafumi24@users.noreply.github.com>
2020-07-26 11:55:18 +0900

    vue.js todoapp

---
+++ js/sample.js
@@ -0,0 +1,15 @@
+var app = new Vue ({
+  el: '#app',
+  data: { 
+    newItem: '',
+    todos: []
+  },
+  methods: {
+    addItem: function(event) {
+      var todo = {
+        item: this.newItem
+      };
+      this.todos.push(todo)
+    }
+  }
+})
